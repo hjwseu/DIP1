dd2016cab1a468b20ccfbdb58bb89c6bb62f5a10
hjwseu <hjwseu@outlook.com>
2019-09-11 19:53:42 +0800

    完成了1.2和1.1的修正

--- main.cpp
+++ main.cpp
@@ -11,23 +11,28 @@ int main()
 	//calculate M
 	MatrixXf matrix2f_a = matrix2d_a.cast<float>(); //Convert an int matrix to a float matrix
 	MatrixXf matrix2f_b = matrix2d_b.cast<float>();
-	MatrixXf M = matrix2f_a.jacobiSvd(ComputeThinU | ComputeThinV).solve(matrix2f_b);
-	cout << M <<endl;
-	
+
+	//Homogeneous
+	MatrixXf matrix2f_a_homo(16, 3);
+	MatrixXf mat_one_1(16, 1);
+	mat_one_1.setOnes();
+	matrix2f_a_homo << matrix2f_a, mat_one_1;
+
+	//calculate M'
+	MatrixXf M = matrix2f_a_homo.jacobiSvd(ComputeThinU | ComputeThinV).solve(matrix2f_b);
+
+	//Transpose M' to M
+	M.transposeInPlace();
+	cout << M << endl;
+
 	Mat input_img_a = imread("./material/A.png");
 	Mat input_img_b = imread("./material/B.png");
 	Mat mat_M,output_img;
 	eigen2cv(M, mat_M);
 
-	//registration
-	copyMakeBorder(mat_M, mat_M, 0, 0, 0, 1, BORDER_CONSTANT, Scalar(0));
-	cout << mat_M << endl;
 	warpAffine(input_img_a, output_img, mat_M, output_img.size());
 
-	//imshow("input_img_a", input_img_a);
-	//imshow("output_a", output_img);
-	//imshow("input_img_b", input_img_b);
-	//imwrite("ouputimg_points2d.png", output_img);
+	imwrite("ouputimg_points2d_1.png", output_img);
 	//*****************************************************
 
 	//*****************************************************
@@ -44,15 +49,29 @@ int main()
 	MatrixXf matrix3f_a = txt2matrix_3(points3d_a_addr);
 	MatrixXf matrix3f_b = txt2matrix_3(points3d_b_addr);
 
+	//Homogenous
+	MatrixXf matrix3f_a_homo(16, 4);
+	MatrixXf matrix3f_b_homo(16, 4);
+	matrix3f_a_homo << matrix3f_a, mat_one;
+	matrix3f_b_homo << matrix3f_b, mat_one;
+
 	//K
 	MatrixXf K(3,3);
 	K << 746.07, 0, 493.94,
 		0, 743.92, 488.76,
 		0, 0, 1;
-	MatrixXf Sa = mat2d_homo_a.jacobiSvd(ComputeThinU | ComputeThinV).solve(matrix3f_a*K);
-	MatrixXf Sb = mat2d_homo_b.jacobiSvd(ComputeThinU | ComputeThinV).solve(matrix3f_b*K);
-	cout << "Sa is " << endl<< Sa <<endl;
-	cout << "Sb is " << endl << Sb << endl;
+	//inverse K and transform K^-1
+	K.inverse();
+	K.transposeInPlace();
+
+	//calculate 
+	MatrixXf RT_a = matrix3f_a_homo.jacobiSvd(ComputeThinU | ComputeThinV).solve(mat2d_homo_a *K);
+	MatrixXf RT_b = matrix3f_b_homo.jacobiSvd(ComputeThinU | ComputeThinV).solve(mat2d_homo_b *K);
+
+	RT_a.transposeInPlace();
+	RT_b.transposeInPlace();
+	cout << "RT_a is " << endl << RT_a << endl;
+	cout << "RT_b is " << endl << RT_b << endl;
 
 	////calculate S
 	//MatrixXf matrix3f_a = matrix3d_a.cast<float>(); //Convert an int matrix to a float matrix
